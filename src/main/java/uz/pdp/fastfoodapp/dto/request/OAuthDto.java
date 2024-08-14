db532f5271a98186f9270f40b01e392b4a1d7363
ツ <javaguru1716@gmail.com>
2024-08-14 18:55:01 +0500

    Validation is done

--- src/main/java/uz/pdp/fastfoodapp/dto/request/OAuthDto.java
+++ src/main/java/uz/pdp/fastfoodapp/dto/request/OAuthDto.java
@@ -2,10 +2,12 @@ package uz.pdp.fastfoodapp.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import uz.pdp.fastfoodapp.util.annotations.Email;
 
 @AllArgsConstructor
 @Getter
 public class OAuthDto {
+    @Email
     private String email;
     private String name;
 }
