15e7f0151c63c91b6857e3d00525b8fccdbb3307
ツ <javaguru1716@gmail.com>
2024-08-15 14:36:40 +0500

    Profile is done

---
+++ src/main/java/uz/pdp/fastfoodapp/util/validator/Validator.java
@@ -0,0 +1,29 @@
+package uz.pdp.fastfoodapp.util.validator;
+
+import java.util.List;
+
+public class Validator {
+    public static boolean isNullOrEmpty(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
+    public static <T> T requireNonNullElse(T obj, T defaultValue) {
+        if (obj == null) {
+            return defaultValue;
+        }
+        if (obj instanceof String s) {
+            if (isNullOrEmpty(s)) {
+                return defaultValue;
+            }
+        } else if (obj instanceof Integer || obj instanceof Double) {
+            double number = (obj instanceof Integer) ? ((Integer) obj).doubleValue() : (Double) obj;
+            if (number < 0) {
+                return defaultValue;
+            }
+        } else if (obj instanceof List<?> list) {
+            if (list.isEmpty())
+                return defaultValue;
+        }
+        return obj;
+    }
+}
